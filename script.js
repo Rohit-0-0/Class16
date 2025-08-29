7bf8f8571e05ca916921eb7c3b9d387f63c6de95
Rohit-0-0 <rohitpatel90878@gmail.com>
2025-08-29 11:06:38 +0530

    second commit

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 //Arrays and its methods pop push find filter map etc
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,10];
 //find method
-const find = arr.find((value) => {
-    return value > 10;
-})
-console.log(find);
+// const find = arr.find((value) => {
+//     return value > 10;
+// })
+// console.log(find);
 //filter method
-const filter = arr.filter((value) => {
-    return value > 10;
-})
-console.log(filter);
+// const filter = arr.filter((value) => {
+//     return value > 10;
+// })
+// console.log(filter);
 //map method
-const map = arr.map((value) => {
-    return value * 2;
-})
-console.log(map);
+// const map = arr.map((value) => {
+//     return value * 2;
+// })
+// console.log(map);
 //reduce method
-const reduce = arr.reduce((acc, value) => {
-    return acc + value;
-}, 0)
-console.log(reduce);
+// const reduce = arr.reduce((acc, value) => {
+//     return acc + value;
+// }, 0)
+// console.log(reduce);
 //forEach
-arr.forEach((value) => {
-    console.log(value);
+// arr.forEach((value) => {
+//     console.log(value);
     // console.log(value * 2);
-})
+// })
 //slice 
 // const slice = arr.slice(3, 7);
 // console.log(slice);
@@ -34,6 +34,30 @@ arr.forEach((value) => {
 // 
 console.log(arr)
 //flat
-let arr1=[1,2,3,[1,2,3],1,2,3]
-let arr2=arr1.flat()
-console.log(arr2);
+// let arr1=[1,2,3,[1,2,3],1,2,3]
+// let arr2=arr1.flat()
+// console.log(arr2);
+//push
+// const push = arr.push(20)
+// console.log(arr);
+//pop
+// const pop = arr.pop()
+// console.log(arr);
+//shift
+// const shift = arr.shift()
+// console.log(arr);
+//unshift
+// const unshift = arr.unshift(10)
+// console.log(arr);
+//includes
+// const includes = arr.includes(10)
+// console.log(includes);
+//indexOf
+// const indexOf = arr.indexOf(10)
+// console.log(indexOf);
+//lastIndexOf
+// const lastIndexOf = arr.lastIndexOf(10)
+// console.log(lastIndexOf);
+//join
+const join = arr.join(" ")
+console.log(join);
